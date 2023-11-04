eea642bcf05c7d061be4b9d7e12f58c62b2f37c8
Nick McLean <nickisyourfan@gmail.com>
2023-11-04 16:41:57 -0500

    local time

--- src/pages/todos/view/components/goaldate-alert/index.tsx
+++ src/pages/todos/view/components/goaldate-alert/index.tsx
@@ -10,7 +10,7 @@ export const GoalDateAlert: FC<{
   visible && (
     <Alert color="info" className="mb-4">
       <h3 className="text-lg font-bold">
-        Due: {dayjs.tz(todo?.goal_date).format("MMM DD, YYYY h:mm A")}
+        Due: {dayjs.tz(todo?.goal_date).local().format("MMM DD, YYYY h:mm A")}
       </h3>
       <p>You have {dayjs.tz(todo?.goal_date).fromNow()} to complete it.</p>
     </Alert>
